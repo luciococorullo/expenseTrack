cf9686be75eacba0314e1ce8a92a85490ab1ed18
luciococorullo <luciococorullo17@gmail.com>
2022-12-20 22:20:45 +0100

    Initial Commit

---
+++ expenseTrack/expenseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseTrackApp.swift
+//  expenseTrack
+//
+//  Created by Lucio Cocorullo on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct expenseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
